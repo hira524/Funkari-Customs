2ce87c3a104bb5a713d52208578f90f97c289a97
syeda hira <syedahira2846@gmail.com>
2025-07-02 09:55:16 +0500

    update

--- Main/anvogue/src/app/product/styles/style5/page.tsx
+++ Main/anvogue/src/app/product/styles/style5/page.tsx
@@ -1,25 +1,45 @@
 'use client'
-
-import React, { useState } from 'react'
-import { useRouter, useSearchParams } from 'next/navigation';
-import TopNavOne from '@/components/Header/TopNav/TopNavOne'
-import MenuOne from '@/components/Header/Menu/MenuOne'
-import ShopFilterCanvas from '@/components/Shop/ShopFilterCanvas'
-import productData from '@/data/Product.json'
-import Footer from '@/components/Footer/Footer'
+import React, { Suspense } from 'react';
+import TopNavOne from '@/components/Header/TopNav/TopNavOne';
+import MenuOne from '@/components/Header/Menu/MenuOne';
+import ShopFilterCanvas from '@/components/Shop/ShopFilterCanvas';
+import productData from '@/data/Product.json';
+import Footer from '@/components/Footer/Footer';
 
 export default function FilterCanvasProductFive() {
-    const searchParams = useSearchParams()
-    const type = searchParams.get('type')
+  return (
+    <>
+      <TopNavOne
+        props="style-one bg-black"
+        slogan="New customers save 10% with the code GET10"
+      />
+      <div id="header" className="relative w-full">
+        <MenuOne props="bg-transparent" />
+      </div>
 
-    return (
-        <>
-            <TopNavOne props="style-one bg-black" slogan="New customers save 10% with the code GET10" />
-            <div id="header" className='relative w-full'>
-                <MenuOne props="bg-transparent" />
-            </div>
-            <ShopFilterCanvas data={productData} productPerPage={12} dataType={type} productStyle='style-5' />
-            <Footer />
-        </>
-    )
+      {/* Wrapped in Suspense to fix useSearchParams build error */}
+      <Suspense fallback={<div>Loading products...</div>}>
+        <ClientFilterCanvas />
+      </Suspense>
+
+      <Footer />
+    </>
+  );
 }
+
+// ✅ Nested client-side component to use useSearchParams
+function ClientFilterCanvas() {
+  'use client';
+  const searchParams = useSearchParams();
+  const type = searchParams.get('type');
+
+  return (
+    <ShopFilterCanvas
+      data={productData}
+      productPerPage={12}
+      dataType={type}
+      productStyle="style-5"
+    />
+  );
+}
+import { useSearchParams } from 'next/navigation';
